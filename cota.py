ce55755da6ae908f10ed7a0c84ad4bfb102995fe
Miguel Johnson <miguelbjohnson@gmail.com>
2021-03-07 18:55:03 -0500

    Pulled Vehicle series from JSON

--- cota.py
+++ cota.py
@@ -2,13 +2,23 @@ import streamlit as st
 import numpy as np 
 import pandas as pd
 
+# Setting up page.
 st.title('COTA Live Update App')
+st.write('This app will ingest streamed data from the Smart Columbus website and show real-time COTA bus locations.')
+st.subheader('Vehicle Series from Location DataFrame')
 
-st.write(
-    'This app will ingest streamed data from the Smart Columbus website and show real-time COTA bus locations.'
-    
-)
+location_df =  pd.read_json('query_results.json')
+trips = location_df['vehicle']
 
 
+
+st.write(trips)
+
+
+#Create map on webpage
 st.map()
+
+
+
+
 
