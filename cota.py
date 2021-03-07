953647f25ab5a5e86070f22428f2a522f1400013
Miguel Johnson <miguelbjohnson@gmail.com>
2021-03-07 14:45:08 -0500

    Streamlit app has been set up locally with title

---
+++ cota.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd
+
+st.title('COTA Live Update App')
+
+st.write(
+    'This app will ingest streamed data from the Smart Columbus website and show real-time COTA bus locations.'
+    
+)
+
+
+st.map()
+
