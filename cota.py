dbd9026685be4f99b372ea1bc0b0aca1ec349291
Miguel Johnson <miguelbjohnson@gmail.com>
2021-03-10 01:00:49 -0500

    Adding ipynb file for small tests - added bus id

--- cota.py
+++ cota.py
@@ -7,13 +7,17 @@ from cota_app_interface import *
 location_df =  pd.read_json('query_results.json')
 trips = location_df['vehicle']
 
+st.write(trips)
+
 def get_locations(trips):
     for trip in trips:
         lat = trip['position']['latitude']
         long = trip['position']['longitude']
-        location = lat,long
+        route_id = trip['trip']['route_id']
+        location = route_id,lat,long
         
         st.write(location)
+
 
 get_locations(trips)
 
